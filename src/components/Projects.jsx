60fa7b37328f6daceaefb97c1cea1d039a8452dc
r-yvan <yvankiliye.rubuto@gmail.com>
2025-01-05 09:42:03 +0200

    Commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,8 @@ const Projects = () => {
     },
     {
       title: "PicDeck",
-      description: "Modern web app build for artists and other art enthusiasts to visualize their work..",
+      description:
+        "Modern web app build for artists and other art enthusiasts to visualize their work..",
       img: {},
     },
     {
@@ -35,30 +36,34 @@ const Projects = () => {
   ];
 
   return (
-    <div className="pb-10 overflow-hidden relative pt-10 bg-[#0a0a0a] text-white z-30">
+    <div className="pb-10 w-full overflow-hidden relative pt-10 bg-[#0a0a0a] text-white z-30">
       <div className="flex spotlight-2 justify-center z-20">
         <h2 className="text-6xl font-bold mb-20 z-20">
           My <span className="text-green-400">Projects</span>
         </h2>
       </div>
-      <div className="flex flex-row flex-wrap justify-center for-scrollbar gap-5 px-20 z-20">
-        {projects.map((project, index) => (
-          <div
-            className="flex flex-col overflow-y-auto w-card flex-shrink-0 for-scrollbar bg-[#0a0a0a] backdrop-blur-3xl border border-white border-opacity-10 duration-200 ease-out hover:scale-95 hover:shadow-custom hover:border-green-400 hover:border-opacity-100 rounded-xl pb-2 z-20"
-            key={index}
-          >
-            <div className="w-full rounded-t-xl overflow-hidden mb-3">
-              <img src={temp} alt="" />
+      <div className="flex justify-center w-full">
+        <div className="flex flex-row flex-wrap w-[1300px] for-scrollbar gap-5 z-20">
+          {projects.map((project, index) => (
+            <div
+              className="flex flex-col overflow-y-auto w-card flex-shrink-0 for-scrollbar bg-[#151515] backdrop-blur-3xl border border-white border-opacity-10 duration-200 ease-out hover:scale-95 hover:shadow-custom hover:border-green-400 hover:border-opacity-100 rounded-xl pb-2 z-20"
+              key={index}
+            >
+              <div className="w-full rounded-t-xl overflow-hidden mb-2">
+                <img src={temp} alt="" />
+              </div>
+              <div className="flex flex-col h-28 pr-2 pl-2 before:contents-[''] before:absolute before:w-1 before:h-28 before:bg-green-400 before:rounded-full">
+                <h3 className="pl-3 text-lg font-semibold mb-1">
+                  {project.title}
+                </h3>
+                <p className="pl-3 text-white text-opacity-50">{project.description}</p>
+              </div>
+              <div className="absolute right-1 bottom-2 h-28 w-28">
+                <img src={upt} alt="" />
+              </div>
             </div>
-            <div className="flex flex-col h-28 pr-2 pl-2 before:contents-[''] before:absolute before:w-1 before:h-28 before:bg-green-400 before:rounded-full">
-              <h3 className="pl-3 text-xl font-semibold mb-4">{project.title}</h3>
-              <p className="pl-3 text-gray-400">{project.description}</p>
-            </div>
-            <div className="absolute right-1 bottom-2 h-28 w-28">
-              <img src={upt} alt="" />
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
